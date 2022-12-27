1723cdc3c11505effc4b91b2714eb954cc0ee457
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-12-27 20:23:00 +0600

    post data send server successfull

--- src/Page/CreatePost/CreatePost.js
+++ src/Page/CreatePost/CreatePost.js
@@ -1,21 +1,48 @@
 import React from 'react';
 
 const CreatePost = () => {
+
+const submitPost= (event)=> {
+     event.preventDefault()
+     const form = event.target;
+     const postText = form.postText.value;
+     console.log(postText)
+
+     const postWords = {
+          postText
+     }
+
+     fetch('http://localhost:5000/posts', {
+          method:'POST',
+          headers:{
+               'content-type': 'application/json'
+          },
+          body: JSON.stringify(postWords)
+     })
+     .then(res => res.json())
+     .then(data => console.log(data))
+     .catch(error => {
+          console.log(error)})
+}
+     
+
      return (
           <div className='border-2 mx-2'>
-               <div className='flex justify-around mt-8'>
-                    {/* <img src="" alt="" /> */}
-                    <p> user img</p>
-                    <textarea name="" id="" className=' w-3/4 border p-1' placeholder='Whats in your mind? '></textarea>
-               </div>
-               <div className='flex justify-around my-5'>
-                    <div>
-                         <label htmlFor="postImg"  className='w-28 border px-2 py-1'>img + video</label>
-                         <input type="file" id='postImg' className="file-input file-input-bordered file-input-info hidden " />
+               <form onSubmit={submitPost}>
+                    <div className='flex justify-around mt-8'>
+                         {/* <img src="" alt="" /> */}
+                         <p> user img</p>
+                         <textarea name="postText" id="" className=' w-3/4 border p-1' placeholder='Whats in your mind? '></textarea>
                     </div>
+                    <div className='flex justify-around my-5'>
+                         <div>
+                              <label htmlFor="postImg" className='w-28 border px-2 py-1'>img + video</label>
+                              <input type="file" id='postImg' className="file-input file-input-bordered file-input-info hidden " />
+                         </div>
 
-                    <button className='btn btn-info'> Post </button>
-               </div>
+                         <input className='btn btn-info' type="submit" name="post" id="" value='Post' />
+                    </div>
+               </form>
           </div>
      );
 };
